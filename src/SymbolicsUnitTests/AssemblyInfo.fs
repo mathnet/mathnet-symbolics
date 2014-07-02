c378f7d9f7e92f7f56f57883a08c2edeb499b61e
Christoph Ruegg <git@cdrnet.ch>
2014-07-02 15:10:53 +0200

    Clarify current alpha status

--- src/SymbolicsUnitTests/AssemblyInfo.fs
+++ src/SymbolicsUnitTests/AssemblyInfo.fs
@@ -11,6 +11,6 @@ open System.Runtime.InteropServices
 
 [<assembly: AssemblyVersion("0.1.0.0")>]
 [<assembly: AssemblyFileVersion("0.1.0.0")>]
-[<assembly: AssemblyInformationalVersion("0.1.0")>]
+[<assembly: AssemblyInformationalVersion("0.1.0-alpha")>]
 
 ()
