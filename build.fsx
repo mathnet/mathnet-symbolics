bcad5a18c9ec6c5c63d1057c4eafaff39bd16263
Christoph Ruegg <git@cdrnet.ch>
2014-09-03 14:55:20 +0200

    Add explicit FSharp.Core NuGet dependency, whitelist core dependencies

--- build.fsx
+++ build.fsx
@@ -216,7 +216,7 @@ let zip zipDir filesDir filesFilter bundle =
 
 Target "Zip" (fun _ ->
     CleanDir "out/packages/Zip"
-    coreBundle |> zip "out/packages/Zip" "out/lib" (fun f -> f.Contains("MathNet.Symbolics.") || f.Contains("MathNet.Numerics.") || f.Contains("FParsec")))
+    coreBundle |> zip "out/packages/Zip" "out/lib" (fun f -> f.Contains("MathNet.Symbolics.") || f.Contains("MathNet.Numerics.") || f.Contains("FParsec") || f.Contains("FSharp.Core")))
 "Build" ==> "Zip"
 
 
