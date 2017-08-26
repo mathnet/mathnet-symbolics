03476b5905497d33fc86b6432711022f4c952cda
Christoph Ruegg <git@cdrnet.ch>
2017-08-26 14:41:00 +0200

    SymbolicExpression: casting, basic operators

--- src/Symbolics/SymbolicExpression.fs
+++ src/Symbolics/SymbolicExpression.fs
@@ -73,5 +73,65 @@ type SymbolicExpression(expression:Expression) =
 
 
     // EVALUATION & COMPILATION
+
+
+    // CASTING
+
+    static member op_Implicit (x:Expression) : SymbolicExpression = SymbolicExpression(x)
+    static member op_Implicit (x:string) : SymbolicExpression = SymbolicExpression.Parse(x)
+
+    static member op_Implicit (x:int32) : SymbolicExpression = SymbolicExpression.FromInt32(x)
+    static member op_Implicit (x:int64) : SymbolicExpression = SymbolicExpression.FromInt64(x)
+    static member op_Implicit (x:BigInteger) : SymbolicExpression = SymbolicExpression.FromInteger(x)
+    static member op_Implicit (x:BigRational) : SymbolicExpression = SymbolicExpression.FromRational(x)
+    static member op_Implicit (x:float) : SymbolicExpression = SymbolicExpression.Real(x)
+
+    // bad idea, don't do this
+    // static member op_Implicit (x:SymbolicExpression) : Expression = x.Expression
+
+
     // OPERATORS
-    // ...
+
+    static member ( ~+ ) (x:SymbolicExpression) = SymbolicExpression(+x.Expression)
+    static member ( ~- ) (x:SymbolicExpression) = SymbolicExpression(-x.Expression)
+    static member ( + ) ((x:SymbolicExpression), (y:SymbolicExpression)) = SymbolicExpression(x.Expression + y.Expression)
+    static member ( - ) ((x:SymbolicExpression), (y:SymbolicExpression)) = SymbolicExpression(x.Expression - y.Expression)
+    static member ( * ) ((x:SymbolicExpression), (y:SymbolicExpression)) = SymbolicExpression(x.Expression * y.Expression)
+    static member ( / ) ((x:SymbolicExpression), (y:SymbolicExpression)) = SymbolicExpression(x.Expression / y.Expression)
+
+    static member Sum([<System.ParamArray>] summands : SymbolicExpression array) = SymbolicExpression(summands |> Seq.map (fun x -> x.Expression) |> Operators.sumSeq)
+    static member Sum(summands : SymbolicExpression seq) = SymbolicExpression(summands |> Seq.map (fun x -> x.Expression) |> Operators.sumSeq)
+    static member Product([<System.ParamArray>] factors : SymbolicExpression array) = SymbolicExpression(factors |> Seq.map (fun x -> x.Expression) |> Operators.productSeq)
+    static member Product(factors : SymbolicExpression seq) = SymbolicExpression(factors |> Seq.map (fun x -> x.Expression) |> Operators.productSeq)
+
+    member this.Negate() = -this
+    member this.Add(x:SymbolicExpression) = this + x
+    member this.Subtract(x:SymbolicExpression) = this - x
+    member this.Multiply(x:SymbolicExpression) = this * x
+    member this.Divide(x:SymbolicExpression) = this / x
+
+    member this.Pow(power:SymbolicExpression) = SymbolicExpression(Expression.Pow(expression, power.Expression))
+    member this.Invert() = SymbolicExpression(Expression.Invert(expression))
+
+    member this.Abs() = SymbolicExpression(Expression.Abs(expression))
+
+    member this.Root(n:SymbolicExpression) = SymbolicExpression(Expression.Root(n.Expression, expression))
+    member this.Sqrt() = SymbolicExpression(Expression.Sqrt(expression))
+
+    member this.Exp() = SymbolicExpression(Expression.Exp(expression))
+    member this.Ln() = SymbolicExpression(Expression.Ln(expression))
+    member this.Log() = SymbolicExpression(Expression.Log(expression))
+    member this.Log(basis:SymbolicExpression) = SymbolicExpression(Expression.Log(basis.Expression, expression))
+
+    member this.Sin() = SymbolicExpression(Expression.Sin(expression))
+    member this.Cos() = SymbolicExpression(Expression.Cos(expression))
+    member this.Tan() = SymbolicExpression(Expression.Tan(expression))
+    member this.Sec() = SymbolicExpression(Expression.Sec(expression))
+    member this.Csc() = SymbolicExpression(Expression.Csc(expression))
+    member this.Cot() = SymbolicExpression(Expression.Cot(expression))
+    member this.Sinh() = SymbolicExpression(Expression.Sinh(expression))
+    member this.Cosh() = SymbolicExpression(Expression.Cosh(expression))
+    member this.Tanh() = SymbolicExpression(Expression.Tanh(expression))
+    member this.ArcSin() = SymbolicExpression(Expression.ArcSin(expression))
+    member this.ArcCos() = SymbolicExpression(Expression.ArcCos(expression))
+    member this.ArcTan() = SymbolicExpression(Expression.ArcTan(expression))
