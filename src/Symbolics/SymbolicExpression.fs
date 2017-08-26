9346d956d53c429363f70dcfe7754b1e979665fb
Christoph Ruegg <git@cdrnet.ch>
2017-08-26 11:41:57 +0200

    Introducing OOP-friendly SymbolicExpression wrapper

---
+++ src/Symbolics/SymbolicExpression.fs
@@ -0,0 +1,47 @@
+﻿namespace MathNet.Symbolics
+
+open System.Numerics
+open MathNet.Numerics
+open MathNet.Symbolics
+
+type SymbolicExpression(expression:Expression) =
+
+    member this.Expression = expression
+
+
+    // PARSING
+
+    static member Parse(infix:string) : SymbolicExpression =
+        SymbolicExpression(Infix.parseOrThrow infix)
+
+    static member ParseMathML(mathml:string) : SymbolicExpression =
+        SymbolicExpression(MathML.parse mathml)
+
+    static member ParseExpression(expression:System.Linq.Expressions.Expression) : SymbolicExpression =
+        SymbolicExpression(Linq.parse expression)
+
+    static member ParseQuotation(quotation:Microsoft.FSharp.Quotations.Expr) : SymbolicExpression =
+        SymbolicExpression(Quotations.parse quotation)
+
+
+    // FORMATTING
+
+    override this.ToString() : string =
+        Infix.format expression
+
+    member this.ToInternalString() : string =
+        Infix.formatStrict expression
+
+    member this.ToLaTeX() : string =
+        LaTeX.format expression
+
+    member this.ToMathML() : string =
+        MathML.formatContentStrict expression
+
+    member this.ToSemanticMathML() : string =
+        MathML.formatSemanticsAnnotated expression
+
+
+    // EVALUATION & COMPILATION
+    // OPERATORS
+    // ...
