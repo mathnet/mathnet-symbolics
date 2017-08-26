0ce0f68c2228ae23f256dc1cc637ec9f25587b48
Christoph Ruegg <git@cdrnet.ch>
2017-08-26 12:05:18 +0200

    SymbolicExpression: leaf builders

--- src/Symbolics/SymbolicExpression.fs
+++ src/Symbolics/SymbolicExpression.fs
@@ -9,6 +9,35 @@ type SymbolicExpression(expression:Expression) =
     member this.Expression = expression
 
 
+    // LEAFS - Integer
+    static member Zero = SymbolicExpression(Expression.Zero)
+    static member One = SymbolicExpression(Expression.One)
+    static member Two = SymbolicExpression(Expression.Two)
+    static member MinusOne = SymbolicExpression(Expression.MinusOne)
+    static member FromInt32(x:int32) = SymbolicExpression(Expression.FromInt32(x))
+    static member FromInt64(x:int64) = SymbolicExpression(Expression.FromInt64(x))
+    static member FromInteger(x:BigInteger) = SymbolicExpression(Expression.FromInteger(x))
+    static member FromIntegerFraction(n:BigInteger, d:BigInteger) = SymbolicExpression(Expression.FromIntegerFraction(n, d))
+    static member FromRational(x:BigRational) = SymbolicExpression(Expression.FromRational(x))
+
+    // LEAFS - Approximations
+    static member Real(approximation:float) = SymbolicExpression(Expression.Real(approximation))
+
+    // LEAFS - Constants
+    static member I = SymbolicExpression(Expression.I)
+    static member E = SymbolicExpression(Expression.E)
+    static member Pi = SymbolicExpression(Expression.Pi)
+
+    // LEAFS - Mathematical Symbols
+    static member PositiveInfinity = SymbolicExpression(Expression.PositiveInfinity)
+    static member NegativeInfinity = SymbolicExpression(Expression.NegativeInfinity)
+    static member ComplexInfinity = SymbolicExpression(Expression.ComplexInfinity)
+    static member Undefined = SymbolicExpression(Expression.Undefined)
+
+    // LEAFS - Symbols
+    static member Variable(name:string) = SymbolicExpression(Expression.Symbol(name))
+
+
     // PARSING
 
     static member Parse(infix:string) : SymbolicExpression =
