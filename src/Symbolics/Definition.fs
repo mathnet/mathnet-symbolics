3822c760a6920be5fc6eab2b0d20725a3e5c8e71
Your Name <you@example>
2022-02-27 01:11:36 +0800

    customized_function

---
+++ src/Symbolics/Definition.fs
@@ -0,0 +1,12 @@
+﻿namespace MathNet.Symbolics
+
+module Definition =
+
+    let funDict = new System.Collections.Concurrent.ConcurrentDictionary<string, (Symbol list) * Expression>()
+
+    let define fnm exp =
+        funDict.AddOrUpdate(
+            fnm
+            , (fun nm -> exp)
+            , (fun nm cur_exp -> exp)
+        )
